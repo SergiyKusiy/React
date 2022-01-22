65c3d501000263108749ee06be1711429a539c2b
SergiyKusiy <81525113+SergiyKusiy@users.noreply.github.com>
2022-01-22 20:59:30 +0200

    React

---
+++ lesson1/task2/index.js
@@ -0,0 +1,36 @@
+const rootElement = document.querySelector('#root');
+
+// const renderGreeting = elem => {
+//   const containerElem = document.createElement('div');
+//   containerElem.classList.add('greeting');
+
+//   const titleElem = document.createElement('h1');
+//   titleElem.classList.add('greeting__title');
+//   titleElem.textContent = 'Hello, world!';
+
+//   const textElem = document.createElement('p');
+//   textElem.classList.add('greeting__text');
+//   textElem.textContent = "I'm learning React";
+
+//   containerElem.append(titleElem, textElem);
+
+//   elem.append(containerElem);
+// };
+
+const renderGreeting = React.createElement(
+   'div',
+   { className: 'greeting' },
+   React.createElement(
+      'div',
+      { className: 'greeting__title' },
+      "Hello word"
+   ),
+   React.createElement(
+      'div',
+      { className: "greeting__text" },
+      "I'm learning React"
+   ),
+);
+
+
+ReactDOM.render(renderGreeting, rootElement)
