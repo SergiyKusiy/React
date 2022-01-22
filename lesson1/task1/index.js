ac7347c719d3e8813c207ec3ddecf8f3ba8a4bc8
SergiyKusiy <81525113+SergiyKusiy@users.noreply.github.com>
2022-01-22 20:33:13 +0200

    React

--- lesson1/task1/index.js
+++ lesson1/task1/index.js
@@ -1,11 +1,9 @@
 const rootElement = document.querySelector('#root');
 
-// Javascript
-// const greetingElem = document.createElement('div');
-// greetingElem.classList.add('greeting');
-// greetingElem.textContent = 'Hello, React';
+const greetingElem = React.createElement(
+'div',
+{ className: 'greeting' },
+'Hello, React!'
+);
 
-// rootElement.append(greetingElem);
-
-const greetingElem = React.createElement('div', { className: 'greeting' }, 'Hello, React!');
-ReactDOM.render(greetingElem, rootElement); // что вставляем(созданный эл) и куда всавляем(в разметке)
+ReactDOM.render(greetingElem, rootElement)
