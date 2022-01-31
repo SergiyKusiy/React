138561cddba2dcf41ae17a7f7251ee136fa8e3c5
SergiyKusiy <81525113+SergiyKusiy@users.noreply.github.com>
2022-01-31 16:41:41 +0200

    react

---
+++ lesson3/task5/src/Profile.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import moment from 'moment';
+
+const Profile = ({ userData }) => {
+   const faormateDate = moment(userData.birthDate).format('DD MMM YY');
+
+   return (
+      <div className='profile'>
+         <div className="profile__name">{`${userData.firstName} ${userData.lastName}`}</div>
+         <div className="profile__birth">{`Was born ${formateDate} in ${userData.birthPlace}`}</div>
+      </div>
+   );
+};
+export default Profile;
