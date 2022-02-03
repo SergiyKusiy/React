c7f0c3ccb85aa2c6dbb78c50512be087726922b2
SergiyKusiy <81525113+SergiyKusiy@users.noreply.github.com>
2022-02-03 16:13:37 +0200

    react

---
+++ lesson4/task2/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Clock from './Clock.jsx';
+
+const App = () => {
+   return (
+      <>
+         <Clock location='NEW YORK' offset={-5} />
+         <Clock location='KYIV' offset={2} />
+         <Clock location='LONDON' offset={0} />
+      </>
+   );
+};
+
+export default App;
