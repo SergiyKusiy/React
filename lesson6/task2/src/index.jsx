186c2f2aac59600858fbaa762f34129b277eff53
SergiyKusiy <81525113+SergiyKusiy@users.noreply.github.com>
2022-02-05 13:23:43 +0200

    react

--- lesson6/task2/src/index.jsx
+++ lesson6/task2/src/index.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './App'
+import Mailbox from './Mailbox.jsx';
 
 const rootElement = document.querySelector('#root');
 
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Mailbox unreadMessages={ ['1'] }/>, rootElement);
