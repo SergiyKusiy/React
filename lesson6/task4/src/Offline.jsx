019a79ca79f5e2ea8b17d16dda0cff81d243c103
SergiyKusiy <81525113+SergiyKusiy@users.noreply.github.com>
2022-02-05 14:14:56 +0200

    react

---
+++ lesson6/task4/src/Offline.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Offline = () => (
+  <div className="status">
+    <span className="status__text">Offline</span>
+    <button className="status__btn">Reconnect</button>
+  </div>
+);
+export default Offline;
---
+++ lesson6/task4/src/Online.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Online = () => (
+  <div className="status">
+    <span className="status__text">All good!</span>
+  </div>
+);
+export default Online;
