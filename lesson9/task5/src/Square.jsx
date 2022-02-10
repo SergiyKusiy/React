1d17005722d77962a8cd022589fcacd5e0e889c8
SergiyKusiy <81525113+SergiyKusiy@users.noreply.github.com>
2022-02-10 21:12:33 +0200

    react

--- lesson9/task5/src/Square.jsx
+++ lesson9/task5/src/Square.jsx
@@ -1,16 +1,11 @@
-import React from "react";
+import React from 'react';
 
-class Square extends React.Component {
+const Square = props => {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+};
 
-   render() {
-     return (
-       <button
-         className="square"
-         onClick={() => this.props.onClick()}>
-         {this.props.value}
-       </button>
-     );
-   }
-}
- 
 export default Square;
