f05a61fdb5dc2ee4873086fc89751b220472d828
SergiyKusiy <81525113+SergiyKusiy@users.noreply.github.com>
2022-02-10 12:24:50 +0200

    react

---
+++ lesson9/task4/src/UsersList.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Filter from './Filter.jsx';
+
+class UsersList extends React.Component {
+  state = {
+    value: '',
+  };
+
+  handelChange = event => {
+    this.setState({
+      value: event.target.value,
+    });
+  };
+  render() {
+    const users = this.props.users.slice();
+    const sortedUserList = users
+      .filter(user => user.name.toUpperCase().includes(this.state.value.toUpperCase()))
+    return (
+      <div>
+        <ul className="users">{sortedUserList}</ul>
+        <Filter
+          onChange={this.handelChange}
+          filterText={this.state.value}
+          count={sortedUserList.length}
+        />
+      </div>
+    );
+  }
+}
+
+export default UsersList;
--- lesson9/task4/src/index.jsx
+++ lesson9/task4/src/index.jsx
@@ -1,8 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import ConnectionStatus from './ConnectionStatus.jsx';
+import UsersList from './UsersList.jsx';
 
 const rootElement = document.querySelector('#root');
-
-ReactDOM.render(<ConnectionStatus />, rootElement);
+const users = [
+  {
+    id: 'id-0',
+    age: 21,
+    name: 'Serhii',
+  },
+  {
+    id: 'id-1',
+    age: 17,
+    name: 'Ivan',
+  },
+  {
+    id: 'id-2',
+    age: 18,
+    name: 'Oleg',
+  },
+  {
+    id: 'id-3',
+    age: 45,
+    name: 'Olya',
+  },
+];
+ReactDOM.render(<UsersList users={users} />, rootElement);
