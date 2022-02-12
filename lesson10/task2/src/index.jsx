9142ed4ffd350480e5f94fc878a7ce7bf94b1b46
SergiyKusiy <81525113+SergiyKusiy@users.noreply.github.com>
2022-02-12 18:03:34 +0200

    react

--- lesson10/task2/src/index.jsx
+++ lesson10/task2/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import Game from './Game.jsx';
+import App from './App.jsx';
 
-ReactDOM.render(<Game />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
