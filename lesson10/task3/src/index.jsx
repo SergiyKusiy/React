4c18ac19a5f45939c673d6e7e5b5945c7e89e208
SergiyKusiy <81525113+SergiyKusiy@users.noreply.github.com>
2022-02-12 23:23:32 +0200

    react

--- lesson10/task3/src/index.jsx
+++ lesson10/task3/src/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import Game from './Game.jsx';
+import Calculator from './Calculator.jsx';
 
-ReactDOM.render(<Game />, document.getElementById('root'));
+const rootElement = document.querySelector('#root');
+
+ReactDOM.render(<Calculator />, rootElement);
