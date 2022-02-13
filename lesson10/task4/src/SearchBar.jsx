ed8a2a4161b518e5ceb29f110cf8683d8868c116
SergiyKusiy <81525113+SergiyKusiy@users.noreply.github.com>
2022-02-13 16:24:03 +0200

    react

--- lesson10/task4/src/SearchBar.jsx
+++ lesson10/task4/src/SearchBar.jsx
@@ -1,22 +1,28 @@
 import React from 'react';
 
-const SearchBar = ({ filterText, onFilterTextChange, onInStockChange, inStockOnly }) => (
-  <form>
-    <input
-      type="text"
-      placeholder="Search..."
-      value={filterText}
-      onChange={e => onFilterTextChange(e.target.value)}
-    />
-    <p>
+const SearchBar = ({ filterText, inStockOnly, onFilterTextChange, onInStockChange }) => {
+  const handleFilterTextChange = event => {
+    onFilterTextChange(event.target.value);
+  };
+
+  const handleInStockChange = event => {
+    onInStockChange(event.target.checked);
+  };
+
+  return (
+    <form>
       <input
-        type="checkbox"
-        checked={inStockOnly}
-        onChange={e => onInStockChange(e.target.checked)}
-      />{' '}
-      Only show products in stock
-    </p>
-  </form>
-);
+        type="text"
+        placeholder="Search..."
+        value={filterText}
+        onChange={handleFilterTextChange}
+      />
+      <p>
+        <input type="checkbox" checked={inStockOnly} onChange={handleInStockChange} /> Only show
+        products in stock
+      </p>
+    </form>
+  );
+};
 
 export default SearchBar;
