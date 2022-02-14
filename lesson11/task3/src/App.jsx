f3e200e70ef609596c39aa9ac1f6fd568740485b
SergiyKusiy <81525113+SergiyKusiy@users.noreply.github.com>
2022-02-14 15:05:16 +0200

    react

---
+++ lesson11/task3/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Expand from './Expand.jsx';
+
+const App = () => {
+   return (
+      <Expand title="Some title">
+         <p>Some content</p>
+      </Expand>
+   );
+}
+
+export default App;
---
+++ lesson11/task3/src/Expand.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class Expand extends Component {
+   state = {
+      isHide: false,
+   };
+
+   toggleBtn = () => {
+      this.setState({ isHide: !this.state.isHide });
+   }
+   
+
+
+   render() {
+      return (
+         <div className="expand border">
+            <div className='expand__header'>
+               <span header__title>{this.props.title}</span>
+               <button onClick={this.toggleBtn} className="expand__toggle-btn">
+                  {this.state.isHide
+                     ? <i className="fas fa-chevron-up">-</i>
+                     : <i className="fas fa-chevron-down">+</i>
+                  }
+               </button>
+            </div>
+            {this.state.isHide ? <div className="expand__content">{this.props.children}</div> : null}
+         </div>
+      );
+   }; 
+}
+export default Expand;
